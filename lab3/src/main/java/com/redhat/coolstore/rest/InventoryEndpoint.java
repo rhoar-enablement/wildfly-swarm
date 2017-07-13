3e686e08abd10fbc95965ba8400676d66f1dbab1
jamesfalkner <schtool@gmail.com>
2017-07-13 16:51:41 -0400

    add additional detail to lab1 to showcase java ee migration, autodetection

--- lab3/src/main/java/com/redhat/coolstore/rest/InventoryEndpoint.java
+++ lab3/src/main/java/com/redhat/coolstore/rest/InventoryEndpoint.java
@@ -9,6 +9,9 @@ import javax.ws.rs.core.MediaType;
 
 import com.redhat.coolstore.model.Inventory;
 import com.redhat.coolstore.service.InventoryService;
+import org.wildfly.swarm.spi.runtime.annotations.ConfigurationValue;
+
+import java.util.Optional;
 
 @Path("/inventory")
 public class InventoryEndpoint {
@@ -16,10 +19,20 @@ public class InventoryEndpoint {
     @Inject
     private InventoryService inventoryService;
 
+    @Inject
+    @ConfigurationValue("stores.closed")
+    private Optional<String> storesClosed;
+
     @GET
     @Path("/{itemId}")
     @Produces(MediaType.APPLICATION_JSON)
     public Inventory getAvailability(@PathParam("itemId") String itemId) {
-        return inventoryService.getInventory(itemId);
+        Inventory i = inventoryService.getInventory(itemId);
+        for (String store : storesClosed.orElse("").split(",")) {
+            if (store.equalsIgnoreCase(i.getLocation())) {
+                i.setQuantity(0);
+            }
+        }
+        return i;
     }
 }
